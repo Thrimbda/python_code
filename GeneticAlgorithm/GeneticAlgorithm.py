47a6b510b6e485f25290bb2e4da64bc28f55cac8
Macpotty <645092284@qq.com>
2016-03-13 16:00:31 +0800

    python_code

--- GeneticAlgorithm/GeneticAlgorithm.py
+++ GeneticAlgorithm/GeneticAlgorithm.py
@@ -2,7 +2,200 @@
 # @Author: Macpotty
 # @Date:   2016-03-12 09:58:53
 # @Last Modified by:   Macpotty
-# @Last Modified time: 2016-03-12 15:07:21
+# @Last Modified time: 2016-03-13 15:58:52
 import numpy as np
 import matplotlib.pyplot as plt
-import random
+import matplotlib.animation as animation
+import copy
+
+# V = np.arange(10)
+# E = np.random.randint(1, 50, size=[10, 10])
+V = list(zip(np.random.random(10)*100, np.random.random(10)*100))
+E = np.zeros([10, 10])
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i == j:
+            E[i, j] = None
+        else:
+            E[i, j] = np.sqrt((V[i][0]-V[j][0])**2+(V[i][1]-V[j][1])**2)
+
+
+class GAUnit:
+    def __init__(self, dimention, bound):
+        self.dimention = dimention
+        self.bound = bound
+        self.fitness = 0
+        self.geneCode = np.zeros(self.dimention, dtype=int)
+        self.length = 0
+
+    # def initGrapg(self):
+    #     V = list(zip(np.random.random(self.imention)*100, np.random.random(self.dimention)*100))
+    #     E = np.zeros([self.dimention, self.dimention])
+    #     for i in range(0, self.dimention):
+    #         for j in range(0, self.dimention):
+    #             if i == j:
+    #                 E[i, j] = None
+    #             else:
+    #                 E[i, j] = np.sqrt((V[i][0]-V[j][0])**2+(V[i][1]-V[j][1])**2)
+
+    def generate(self):
+        for i in range(0, self.dimention):
+            gen = np.random.randint(0, self.dimention - i)
+            self.geneCode[i] = gen
+
+    def geneDecode(self):
+        self.cityIndex = copy.deepcopy(self.geneCode)
+        for i in range(0, self.dimention)[::-1]:
+            for j in range(0, i)[::-1]:
+                if self.cityIndex[j] <= self.cityIndex[i]:
+                    self.cityIndex[i] += 1
+        return self.cityIndex
+
+    def evaluateFitness(self):
+        global E
+        self.geneDecode()
+        for i in range(0, self.dimention):
+            if i == self.dimention - 1:
+                pass
+            else:
+                self.length += E[self.cityIndex[i], self.cityIndex[i+1]]
+        self.fitness = 10/self.length
+
+
+class GA:
+    def __init__(self, sizePop, dimention, bound, maxGen, params):
+        self.sizePop = sizePop
+        self.maxGen = maxGen
+        self.dimention = dimention
+        self.bound = bound
+        self.params = params  #Mutation, crossRate
+        self.population = []
+        self.fitness = np.zeros((self.sizePop, 1))
+        self.trace = np.zeros((self.maxGen, 2))
+        self.genTh = 0
+        self.best = None
+        self.t = 0
+
+    def initUnit(self):
+        for i in range(0, self.sizePop):
+            unit = GAUnit(self.dimention, self.bound)
+            unit.generate()
+            self.population.append(unit)
+
+    def evaluate(self):
+        for i in range(0, self.sizePop):
+            self.population[i].evaluateFitness()
+            self.fitness[i] = self.population[i].fitness
+
+    def selection(self):
+        newPop = []
+        fitnessTotal = np.sum(self.fitness)
+        fitnessSection = np.zeros((self.sizePop, 1))
+
+        temp = 0
+        for i in range(0, self.sizePop):
+            fitnessSection[i] = temp + self.fitness[i]/fitnessTotal
+            temp = fitnessSection[i]
+
+        for i in range(0, self.sizePop):
+            seed = np.random.random()
+            index = 0
+            for j in range(0, self.sizePop - 1):
+                if j == 0 and seed < fitnessSection[j]:
+                    index = 0
+                    break
+                elif seed >= fitnessSection[j] and seed < fitnessSection[j+1]:
+                    index = j+1
+                    break
+            newPop.append(self.population[index])
+        self.population = newPop
+
+    def recombination(self):
+        newPop = []
+        for i in range(0, self.sizePop, 2):
+            index1 = np.random.randint(0, self.sizePop-1)
+            index2 = np.random.randint(0, self.sizePop-1)
+            while index1 == index2:
+                index2 = np.random.randint(0, self.sizePop-1)
+            newPop.append(copy.deepcopy(self.population[index1]))
+            newPop.append(copy.deepcopy(self.population[index2]))
+            seed = np.random.random()
+            if seed < self.params[1]:
+                swapIndex = np.random.randint(1, self.dimention - 2)
+                for j in range(swapIndex, self.dimention - 1):
+                    temp = newPop[i].geneCode[j]
+                    newPop[i].geneCode[j] = newPop[i+1].geneCode[j]
+                    newPop[i+1].geneCode[j] = temp
+                    # swap
+        self.population = newPop
+
+    def mutation(self):
+        newPop = []
+        for i in range(0, self.sizePop):
+            newPop.append(copy.deepcopy(self.population[i]))
+            seed = np.random.random()
+            if seed < self.params[0]:
+                mutateIndex = np.random.randint(0, self.dimention - 1)
+                # alpha = np.random.random()
+                # if alpha < 0.5:
+                newPop[i].geneCode[mutateIndex] = int((self.bound[1, mutateIndex]-mutateIndex)**(1-self.t/self.maxGen))
+                # else:
+                #     newPop[i].geneCode[mutateIndex] = int((self.dimention-mutateIndex)**(1-self.t/self.maxGen))
+        self.population = newPop
+
+    def bigBang(self):
+        self.initUnit()
+        self.evaluate()
+        self.best = np.max(self.fitness), np.argmax(self.fitness), copy.deepcopy(self.population[np.argmax(self.fitness)])
+        self.aveFitness = np.mean(self.fitness)
+        # self.trace[self.t, 0] = (1 - self.best[0])/self.best[0]
+        # self.trace[self.t, 1] = (1 - self.aveFitness)/self.aveFitness
+        while(self.t < self.maxGen - 1):
+            self.t += 1
+            self.selection()
+            self.recombination()
+            self.mutation()
+            self.evaluate()
+            self.best = np.max(self.fitness), np.argmax(self.fitness), copy.deepcopy(self.population[np.argmax(self.fitness)].geneDecode())
+            self.aveFitness = np.mean(self.fitness)
+            # self.trace[self.t, 0] = (1 - self.best[0])/self.best[0]
+            # self.trace[self.t, 1] = (1 - self.aveFitness)/self.aveFitness
+
+            yield self.best
+
+
+class PlotGraph:
+    def __init__(self):
+        global V
+        self.fig = plt.figure(figsize=(10, 10))
+        self.ax = self.fig.add_subplot(111)
+        self.ax.set_xlim(0, 100)
+        self.ax.set_ylim(0, 100)
+        self.ax.set_title("TSP")
+        self.x_data, self.y_data = [], []
+        self.line, = self.ax.plot([], [], 'g-', lw=2)
+
+        bound = np.tile([[0], [9]], 10)
+        self.ga = GA(60, 10, bound, 1000, [0.1, 0.9])
+
+    def init(self):
+        self.line.set_data([], [])
+        return self.line
+
+    def generator(self):
+        self.ga.bigBang()
+        for i in self.ga.best[2]:
+            self.x_data.append(V[i][0])
+            self.y_data.append(V[i][1])
+            yield V[i][0], V[i][1]
+
+    def func(self):
+        self.ax.set_data(self.x_data, self.y_data)
+        return self.line
+
+    def animationInit(self):
+        self.draw = animation.FuncAnimation(self.fig, self.func, self.generator, init_func=self.init, blit=False, interval=0, repeat=False)
+
+if __name__ == '__main__':
+    graph = PlotGraph()
+    graph.animationInit()
--- tset.py
+++ tset.py
@@ -1,16 +1,13 @@
-import matplotlib.pyplot as plt
+import time
 import numpy as np
+import matplotlib.pyplot as plt
 
-x = np.linspace(0, 6*np.pi, 100)
-y = np.sin(x)
-
-# You probably won't need this if you're embedding things in a tkinter plot...
+plt.axis([0, 1000, 0, 1])
 plt.ion()
+plt.show()
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-line1, = ax.plot(x, y, 'r-') # Returns a tuple of line objects, thus the comma
-
-for phase in np.linspace(0, 10*np.pi, 500):
-    line1.set_ydata(np.sin(x + phase))
-    fig.canvas.draw()
+for i in range(1000):
+    y = np.random.random()
+    plt.scatter(i, y)
+    plt.draw()
+    time.sleep(0.05)
