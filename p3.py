e9e8a6f1e2c55b0ef79a9d0dac076f84b7c7df42
Macpotty <645092284@qq.com>
2016-03-19 22:39:01 +0800

    python_code

--- p3.py
+++ p3.py
@@ -1,18 +1,6 @@
-class adder:
-	def __init__(self,first=[]):
-		self.data=first
-	def add(self,x,y):
-		print 'Not Impelmented'
-	def __add__(self,other):
-		return self.add(self.data,other)
-	def __radd__(self,other):
-		return self.add(other,self.data)
-class listadder(adder):
-	def add(self,x,y):
-		return x+y
-class dictadder(adder):
-	def add(self,x,y):
-		new={}
-		new.update(x)
-		new.update(y)
-		return new
+import serial
+
+ser = serial.Serial("/dev/ttyUSB0", 115200)
+
+while True:
+    print(ser.readline())
