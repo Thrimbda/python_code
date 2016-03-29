353a5f7ab2d22d0d194d9cd90646349cd681a1c7
Macpotty <645092284@qq.com>
2016-03-29 20:40:39 +0800

    python_code

--- p4.py
+++ p4.py
@@ -1,12 +1,9 @@
 import os
 import numpy as np
 
-with open('/home/michael/Documents/SORT/SORT2.txt', 'r') as rfileobj:
-    with open(os.path.split(os.path.realpath(__file__))[0] + '/Fmt_SORT2.txt', 'w') as wfileobj:
-        data = rfileobj.readlines()
-        lines = data[1].split(' ')
-        line = '{'
-        for i in lines:
-            line += i
-            line += ','
-        wfileobj.write(line)
+with open('/home/michael/Documents/python_code/RPi_and_BigMonster/Computer/encoder1.txt', 'r') as rfileobj:
+    with open(os.path.split(os.path.realpath(__file__))[0] + '/Fmt_encoder1.txt', 'w') as wfileobj:
+        data = rfileobj.readlines()[274:]
+        for line in data:
+            line = line.replace('(', '').replace(')', '').replace(',', '') + '\n'
+            wfileobj.write(line)
