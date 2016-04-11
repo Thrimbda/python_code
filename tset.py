6d3227510e21159d031e5397ecf0c29c241af8be
Macpotty <645092284@qq.com>
2016-04-11 20:00:01 +0800

    update

--- tset.py
+++ tset.py
@@ -1,50 +1,13 @@
-# -*- coding: utf-8 -*-  
-  
-  
-from scipy import signal  
-import numpy as np  
-import matplotlib.pyplot as pl  
-import matplotlib  
-import math  
-  
-N = 500  
-fs = 5  
-n = [2*math.pi*fs*t/N for t in range(N)]  
-axis_x = np.linspace(0,1,num=N)  
-#设置字体文件，否则不能显示中文  
-myfont = matplotlib.font_manager.FontProperties(fname='c:\\windows\\fonts\\fzshjw_0.ttf')  
-  
-#频率为5Hz的正弦信号  
-x = [math.sin(i) for i in n]  
-pl.subplot(221)  
-pl.plot(axis_x,x)  
-pl.title(u'5Hz的正弦信号', fontproperties=myfont)  
-pl.axis('tight')  
-  
-xx = []  
-x1 = [math.sin(i*10) for i in n]  
-for i in range(len(x)):  
-    xx.append(x[i] + x1[i])  
-   
-pl.subplot(222)  
-pl.plot(axis_x,xx)  
-pl.title(u'5Hz与50Hz的正弦叠加信号', fontproperties=myfont)  
-pl.axis('tight')  
-  
-b,a = signal.butter(3,0.08,'low')  
-sf = signal.filtfilt(b,a,xx)  
-  
-pl.subplot(223)  
-pl.plot(axis_x,sf)  
-pl.title(u'低通滤波后', fontproperties=myfont)  
-pl.axis('tight')  
-  
-b,a = signal.butter(3,0.10,'high')  
-sf = signal.filtfilt(b,a,xx)  
-  
-pl.subplot(224)  
-pl.plot(axis_x,sf)  
-pl.title(u'高通滤波后', fontproperties=myfont)  
-pl.axis('tight')  
+import numpy as np
+import matplotlib.pyplot as plt
 
-pl.show()
+x = np.linspace(-20, 20, 3000, endpoint=True)
+k = np.arange(-100, 100)
+y = []
+
+for i in k:
+    y.append(x ** k)
+
+for i in np.arange(200):
+    plt.plot(x, y[i], color="blue", linewidth=2, linestyle='-')
+plt.show()
