3e6db2e895a9875b7d66d763d33b285fd0bb5747
Macpotty <645092284@qq.com>
2016-07-11 20:00:01 +0800

    daily_buckup

--- TCPClienttest.py
+++ TCPClienttest.py
@@ -1,15 +1,21 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # @Author: Macpotty
 # @Date:   2016-07-07 20:02:56
 # @Last Modified by:   Macpotty
 # @Last Modified time: 2016-07-07 20:44:14
 import socket
+import time
+
 serverName = '120.27.97.44'
 serverPort = 12001
 clienSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clienSocket.connect((serverName, serverPort))
 sentence = input('Input your lowercase sentence:')
 clienSocket.send(sentence.encode())
+timeStamp = time.time()
 modifiedSentence = clienSocket.recv(1024).decode()
-print('From server:', modifiedSentence)
+timeStamp  = time.time() - timeStamp
+print('From server:%s\n' % modifiedSentence, 'Transport using:%.4f' % timeStamp)
 clienSocket.close()
+
