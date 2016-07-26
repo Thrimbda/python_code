89d170e5ce841ae7936035a0a855aac9f0286b0e
Macpotty <645092284@qq.com>
2016-07-26 20:00:02 +0800

    daily_buckup

--- p2.py
+++ p2.py
@@ -1,10 +1,10 @@
 import serial
 import struct
 
-ser = serial.Serial("/dev/ttyACM0", 115200, timeout=0)
+ser = serial.Serial("/dev/ttyUSB1", 115200, timeout=0)
 
 while True:
     x = input()
-    x = eval(x)
-    x = struct.pack("H", x)
-    ser.write(x)
+    # x = eval(x)
+    # x = struct.pack("H", x)
+    ser.write(x.encode())
